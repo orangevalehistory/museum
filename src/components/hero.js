56816d5d4db7c9a9df72b9323f73b53053aac158
witcradg <witcradg@gmail.com>
2023-06-13 13:41:44 -0700

    Hero image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,8 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { getImage } from "gatsby-plugin-image"
-import { BgImage } from "gbimage-bridge"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+// import { BgImage } from "gbimage-bridge" //todo can this be npm removed?
+// import * as styles from './hero.module.css';
 
 //gatsbyImageData(layout: FULL_WIDTH, quality: 50, webpOptions: { quality: 70 })
 /*
@@ -11,44 +12,31 @@ import { BgImage } from "gbimage-bridge"
             formats: [AUTO, WEBP, AVIF]
           )
 */
+
 export const Hero = () => {
-  const { placeholderImage } = useStaticQuery(
-    graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "GRAND_OPENING_ohm.png" }) {
-          childImageSharp {
-            gatsbyImageData(
-              quality: 50
-              webpOptions: { quality: 70 }
-            )
-          }
-        }
-      }
-    `
-  )
-  const pluginImage = getImage(placeholderImage)
+    const { placeholderImage } = useStaticQuery(
+        graphql`
+            query {
+                placeholderImage: file(
+                    relativePath: { eq: "GRAND_OPENING_ohm.png" }
+                ) {
+                    childImageSharp {
+                        gatsbyImageData(
+                            quality: 50
+                            webpOptions: { quality: 70 }
+                        )
+                    }
+                }
+            }
+        `
+    );
+    const backgroundImage = getImage(placeholderImage);
 
-  //BgImage style={{ backgroundPosition: "100% 100%" }}
-
-  return (
-    <div>
-      <BgImage
-        image={pluginImage}
-        className="hero-image text-white flex items-center justify-center"
-        >
-        {/* <div>
-          <div
-            className="text-5xl md:text-7xl 2xl:text-9xl text-center"
-            style={{ lineHeight: "1.25em" }}
-          >
-            The Orangevale <br />
-            History Project
-          </div>
-          <div className="text-sm md:text-lg 2xl:text-2xl text-center font-bold pt-6 pb-2">
-            The official historical society of the Orangevale community
-          </div>
-        </div> */}
-      </BgImage>
-    </div>
-  )
-}
+    return (
+        <div className="pt-1 bg-white">
+            <div className="xl:py-2 w-full flex justify-center align-middle bg-gray-900">
+                <GatsbyImage image={backgroundImage} alt="grand opening" />
+            </div>
+        </div>
+    );
+};
