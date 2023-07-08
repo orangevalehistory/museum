2606b050f710b15af8df02c0b9d8146e67165f5e
witcradg <witcradg@gmail.com>
2023-07-08 15:20:21 -0700

    Change the copyright to reference the OHProject

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,52 +5,47 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+    //   const title =  data.site.siteMetadata?.title || `Title`
+    const title = 'The Orangevale History Project';
 
-  const title =  data.site.siteMetadata?.title || `Title`
-
-  return (
-    <React.Fragment>
-      <Header siteTitle={title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          {/* copyright */}
-          © {new Date().getFullYear()} &middot; {title}
-        </footer>
-      </div>
-    </React.Fragment>
-  )
-}
+    return (
+        <React.Fragment>
+            <Header siteTitle={title} />
+            <div
+                style={{
+                    margin: `0 auto`,
+                    maxWidth: `var(--size-content)`,
+                    padding: `var(--size-gutter)`
+                }}
+            >
+                <main>{children}</main>
+                <footer
+                    style={{
+                        marginTop: `var(--space-5)`,
+                        fontSize: `var(--font-sm)`
+                    }}
+                >
+                    {/* copyright */}
+                    <a
+                        href="https://www.orangevalehistory.org"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        © {new Date().getFullYear()} &middot; {title}
+                    </a>
+                </footer>
+            </div>
+        </React.Fragment>
+    );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
