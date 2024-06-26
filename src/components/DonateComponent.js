87d51695fe0c3991a0d4c6148805cbe3b7b4ac81
witcradg <witcradg@gmail.com>
2024-06-26 13:13:38 -0700

    Add Loading while paypal iframe is retrieved

--- src/components/DonateComponent.js
+++ src/components/DonateComponent.js
@@ -1,10 +1,22 @@
-import * as React from 'react';
+import React,  { useState } from 'react';
 
 export const DonateComponent = () => {
+	const [isLoading, setIsLoading] = useState(true);
+
+	const handleIframeLoad = () => {
+		setIsLoading(false);
+	};
+
 	return (
 		<div className="bg-gray-900 text-white pt-10 md:pb-40 mt-1">
 			<div className="flex justify-center">
+				{isLoading &&
+					<div className="flex justify-center items-center h-screen text-white">
+						Loading...
+					</div>
+				}
 				<iframe
+					onLoad={handleIframeLoad}
 					src="https://www.paypal.com/giving/campaigns?campaign_id=X3MDFKUVAE7FJ"
 					title="PayPal donate campaign card"
 					frameborder="0" width={382} height={550} scrolling="no" />
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,22 +3,10 @@ import Layout from '../components/layout';
 import { DonateComponent } from '../components/DonateComponent';
 
 const DonatePage = () => {
-    const [isLoaded, setIsLoaded] = React.useState(false);
-
-    React.useEffect(() => {
-        setIsLoaded(true);
-    }, []);
-
     return (
         <Layout>
-            {isLoaded ? (
-                <DonateComponent />
-            ) : (
-                <div className="flex justify-center items-center h-screen text-white">
-                    Loading...
-                </div>
-            )}
-        </Layout>
+            <DonateComponent />
+        </Layout >
     );
 };
 
