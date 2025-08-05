f02bdd0faee505dcf7af4a9b768bc9a519bbfc97
witcradg <witcradg@gmail.com>
2025-08-05 15:42:34 -0700

    Update for Night at the Museum donation

--- src/components/DonateComponent.js
+++ src/components/DonateComponent.js
@@ -2,58 +2,20 @@ import React from 'react';
 import campers from '../images/Tour_Campers_2.jpg';
 
 export const DonateComponent = () => {
-
 	return (
-		<div className="bg-gray-900 text-white pt-10 pb-40 mt-1 xl:h-screen relative">
-			<div className={`flex flex-col xl:w-1/2 xl:mx-auto xl:flex-row justify-between gap-4 xl:gap-8`}>
-				<div>
-					<givebutter-widget id="g85DGp"></givebutter-widget>
+		<div className="bg-gray-900 text-white pt-10 pb-40 mt-1 md:h-screen relative">
+			{/* {isLoading && (
+				<div className="flex justify-center items-center h-screen text-white absolute inset-0 bg-gray-900 bg-opacity-75 z-10">
+					Loading...
 				</div>
-				<div id="donation" className='bg-white text-black rounded-lg p-5 h-[420px] w-[358px] mx-auto'>
-					<img src={campers} alt="Docent with campers at the museum"
-						className='max-h-36 w-full' />
-					<div className='text-center text-2xl font-sans mt-4'>
-						<div>Make a year-end donation</div>
-						<form
-							action="https://www.paypal.com/ncp/payment/BEY9DHJE66WNU"
-							method="post"
-							target="_top"
-							style={{
-								display: 'inline-grid',
-								justifyItems: 'center', alignContent: 'start', gap: '1.5rem'
-							}}
-						>
-							<input className="pp-BEY9DHJE66WNU mt-5" type="submit" value="Donate" />
-							<img src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" />
-							<section>
-								Powered by
-								<img
-									src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg"
-									alt="paypal"
-									style={{ height: '0.875rem', verticalAlign: 'middle' }}
-								/>
-							</section>
-						</form>
-						<style>
-							{`
-								.pp-BEY9DHJE66WNU {
-									text-align: center;
-									border: none;
-									border-radius: 1.5rem;
-									min-width: 11.625rem;
-									padding: 0 2rem;
-									height: 2.625rem;
-									font-weight: bold;
-									background-color: #FFD140;
-									color: #000000;
-									font-family: "Helvetica Neue", Arial, sans-serif;
-									font-size: 1rem;
-									line-height: 1.25rem;
-									cursor: pointer;
-								}
-							`}
-						</style>
-					</div>
+			)} */}
+			<div className={`grid grid-cols-1 md:grid-cols-2 lg:flex gap-8 justify-items-center lg:justify-center`}>
+
+				<div>
+					<h1 className="xl:text-3xl m-auto my-4 lg:mt-12 text-center">
+						NIGHT AT THE MUSEUM
+					</h1>
+					<givebutter-widget id="g85DGp" />
 				</div>
 			</div>
 			<div id="non-profit-statement" className="text-center text-xl my-4 py-2">
